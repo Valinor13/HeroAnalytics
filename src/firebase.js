5ecc8040c7b7c7d3e3d2b5d3e511ff9b303d0a2c
Valinor13 <jwcalhoun2@gmail.com>
2022-06-04 11:39:16 -0500

    adding env variables for firebase auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,13 @@
-import firebase from "firebase/compat/app";
-import { getAuth } from "firebase/auth";
+import firebase from 'firebase/compat/app';
+import { getAuth } from 'firebase/auth';
 
 const app = firebase.initializeApp({
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 });
 
 export const auth = getAuth(app);
