2b51a6e591629cd869416e839f8843c0e1e381ec
Valinor13 <jwcalhoun2@gmail.com>
2022-06-12 15:35:06 -0500

    fixing box shadow around folder tab

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,12 @@ import firebase from 'firebase/compat/app';
 import { getAuth } from 'firebase/auth';
 
 const app = firebase.initializeApp({
+  apiKey: "AIzaSyBMbfTHW9Py98NtKNFCr6OBGhqOzuRtGlM",
+  authDomain: "heroxr-development.firebaseapp.com",
+  projectId: "heroxr-development",
+  storageBucket: "heroxr-development.appspot.com",
+  messagingSenderId: "906084342313",
+  appId: "1:906084342313:web:2b66eed05b3c9fcdbe1a38",
 });
 
 export const auth = getAuth(app);
