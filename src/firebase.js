e8d6a79fdf24e43b0ffaeedfefb0cdce7ffff9a1
Jay Calhoun <68342564+Valinor13@users.noreply.github.com>
2022-06-13 13:27:22 -0500

    adding application layer folder

--- src/firebase.js
+++
@@ -1,8 +0,0 @@
-import firebase from 'firebase/compat/app';
-import { getAuth } from 'firebase/auth';
-
-const app = firebase.initializeApp({
-});
-
-export const auth = getAuth(app);
-export default app;
