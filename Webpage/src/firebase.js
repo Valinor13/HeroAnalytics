320bc7461266d807e5cc8cc8f4cce628bd9ebbc7
Jay Calhoun <68342564+Valinor13@users.noreply.github.com>
2022-06-13 13:29:04 -0500

    Create firebase.js

---
+++ Webpage/src/firebase.js
@@ -0,0 +1,8 @@
+import firebase from 'firebase/compat/app';
+import { getAuth } from 'firebase/auth';
+
+const app = firebase.initializeApp({
+});
+
+export const auth = getAuth(app);
+export default app;
